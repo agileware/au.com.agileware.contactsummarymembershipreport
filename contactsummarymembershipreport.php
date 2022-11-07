7ecceccfa9139b7e8db48d347c8d19f9be9c07ea
Justin Freeman <justin@agileware.com.au>
2022-11-07 16:22:46 +1100

    CIVIBLD-287 Re-generate civix

--- contactsummarymembershipreport.php
+++ contactsummarymembershipreport.php
@@ -12,15 +12,6 @@ function contactsummarymembershipreport_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function contactsummarymembershipreport_civicrm_xmlMenu(&$files) {
-  _contactsummarymembershipreport_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -74,54 +65,6 @@ function contactsummarymembershipreport_civicrm_upgrade($op, CRM_Queue_Queue $qu
   return _contactsummarymembershipreport_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function contactsummarymembershipreport_civicrm_managed(&$entities) {
-  _contactsummarymembershipreport_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contactsummarymembershipreport_civicrm_caseTypes(&$caseTypes) {
-  _contactsummarymembershipreport_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function contactsummarymembershipreport_civicrm_angularModules(&$angularModules) {
-  _contactsummarymembershipreport_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function contactsummarymembershipreport_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _contactsummarymembershipreport_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -129,9 +72,8 @@ function contactsummarymembershipreport_civicrm_alterSettingsFolders(&$metaDataF
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function contactsummarymembershipreport_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -149,3 +91,12 @@ function contactsummarymembershipreport_civicrm_navigationMenu(&$menu) {
   ));
   _contactsummarymembershipreport_civix_navigationMenu($menu);
 } // */
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function contactsummarymembershipreport_civicrm_entityTypes(&$entityTypes) {
+  _contactsummarymembershipreport_civix_civicrm_entityTypes($entityTypes);
+}
